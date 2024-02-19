0cd7f0384e607cbf9b6bf7803af96745c686487e
Mona <monanr@stud.ntnu.no>
2024-02-19 15:53:42 +0100

    endre main.c

--- source/main.c
+++ source/main.c
@@ -15,36 +15,41 @@ int main(){
     elevio_motorDirection(DIRN_UP);
 
     while(1){
-        int floor = elevio_floorSensor();
+    //     int floor = elevio_floorSensor();
 
-        if(floor == 0){
-            elevio_motorDirection(DIRN_UP);
-        }
+    //     if(floor == 0){
+    //         elevio_motorDirection(DIRN_UP);
+    //     }
 
-        if(floor == N_FLOORS-1){
-            elevio_motorDirection(DIRN_DOWN);
-        }
+    //     if(floor == N_FLOORS-1){
+    //         elevio_motorDirection(DIRN_DOWN);
+    //     }
 
 
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed);
-            }
-        }
+    //     for(int f = 0; f < N_FLOORS; f++){
+    //         for(int b = 0; b < N_BUTTONS; b++){
+    //             int btnPressed = elevio_callButton(f, b);
+    //             elevio_buttonLamp(f, b, btnPressed);
+    //         }
+    //     }
 
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
+    //     if(elevio_obstruction()){
+    //         elevio_stopLamp(1);
+    //     } else {
+    //         elevio_stopLamp(0);
+    //     }
         
-        if(elevio_stopButton()){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
+    //     if(elevio_stopButton()){
+    //         elevio_motorDirection(DIRN_STOP);
+    //         break;
+    //     }
         
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+    //     nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+
+    if (elevio_callButton(2, 1) == 1) {
+        printf("Hello");
+    }
+
     }
 
     return 0;
